4f9a46c7096ab07d37698558a887805447ee457b
tim mårtensson <tim.martensson92@gmail.com>
2024-10-30 12:18:03 +0100

    added identity, userdatabase and userentity

---
+++ AuthenticationProvider/Models/SignUpModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationProvider.Models;
+
+public class SignUpModel
+{
+    public string Email { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+
+}
