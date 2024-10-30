257216d5279e7ad6bad59d9b6a9688fb5c8c3fb5
tim mårtensson <tim.martensson92@gmail.com>
2024-10-30 16:04:49 +0100

    added tokenservice

--- AuthenticationProvider/Services/AuthService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AuthenticationProvider.Services;
-
-public class AuthService
-{
-
-}
---
+++ AuthenticationProvider/Services/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using AuthenticationProvider.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationProvider.Services;
+
+public class TokenService
+{
+    private readonly string _secretKey;
+    private readonly string _issuer;
+    private readonly string _audience;
+
+    public TokenService(IConfiguration configuration)
+    {
+        _secretKey = configuration["Jwt:SecretKey"]!;
+        _issuer = configuration["Jwt:Issuer"]!;
+        _audience = configuration["Jwt:Audience"]!;
+    }
+
+    public string GenerateJwtToken(UserEntity user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id!),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim("username", user.UserName!),
+            new Claim("isAdmin", user.IsAdmin.ToString())
+        };
+        
+        var token = new JwtSecurityToken(
+            issuer: _issuer,
+            audience: _audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
